702011c3a3cd8436f0b3ef4eb787f9d77dbe1c20
shrois <gercekefendy@gmail.com>
2022-11-01 12:18:15 +0300

    worktile

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,3 +3,4 @@ import NextImage, { ImageProps } from 'next/image'
 const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />
 
 export default Image
+                                                                                                
